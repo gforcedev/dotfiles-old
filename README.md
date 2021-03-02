48a1cd383a25c1a6b2c5e27a43d00d92b6a69a38
gforcedev <tecshotclark@gmail.com>
2021-03-02 22:59:04 +0000

    A clean start

--- README.md
+++
@@ -1,2 +0,0 @@
-# dotfiles
-My configs for things. Currently just the things I need to transfer, might actually set up properly at some point.
--- scripts/fzf/fd.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# fd - cd to selected directory
-fd() {
-  local dir
-  dir=$(rg --hidden --files --null | xargs -0 dirname | uniq | fzf) &&
-  cd "$dir"
-}
